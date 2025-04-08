dc4741b5e0d109f2642086d31f2d41f897a38506
erwinkersten <erwin.kersten@gmail.com>
2025-04-08 18:56:44 +0200

    Add script to generate Kubernetes CSI secrets for dev and prod environments

---
+++ hack/generate-csi-secret.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Get the directory of the script
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
+ENV=$1
+
+# Check if the environment variable is set and valid
+if [[ -z "$ENV" || ! "$ENV" =~ ^(dev|prod)$ ]]; then
+  echo "Usage: $0 [dev|prod]"
+  exit 1
+fi
+
+# Change to the script's directory to ensure relative paths work
+cd "$SCRIPT_DIR/../infratructure/kubernetes" || exit 1
+
+tofu workspace select $ENV || tofu workspace new $ENV
+tofu taint kubernetes_secret.proxmox-csi-plugin-secret
+tofu apply -var-file="environment.${ENV}.tfvars" -target="module.proxmox_csi_plugin.kubernetes_secret.proxmox-csi-plugin-secret"

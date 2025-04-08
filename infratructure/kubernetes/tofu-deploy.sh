e5bac6f7a97b67752ab42f6c7c606bf68d91d83b
erwinkersten <erwin.kersten@gmail.com>
2025-04-08 18:58:23 +0200

    Remove --auto-approve flag from tofu apply command in tofu-deploy.sh

--- infratructure/kubernetes/tofu-deploy.sh
+++ infratructure/kubernetes/tofu-deploy.sh
@@ -8,7 +8,7 @@ if [[ -z "$ENV" || ! "$ENV" =~ ^(dev|prod)$ ]]; then
 fi
 
 tofu workspace select $ENV || tofu workspace new $ENV
-tofu apply -var-file="environment.${ENV}.tfvars" --auto-approve
+tofu apply -var-file="environment.${ENV}.tfvars" 
 export KUBECONFIG="${PWD}/../../config/${ENV}/kube-config.yaml" 
 kubectl get nodes
 echo "Use the following command to use the kubeconfig: export KUBECONFIG=\"${PWD}/../../config/${ENV}/kube-config.yaml\""

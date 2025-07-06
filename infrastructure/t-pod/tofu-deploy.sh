1dc05bf4731f2f85f957b18f9642295ea2a88889
erwinkersten <erwin.kersten@gmail.com>
2025-07-06 18:44:14 +0200

    Add T-Pot setup scripts and Azure infrastructure configuration

---
+++ infrastructure/t-pod/tofu-deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ENV=azure # Default environment is set to 'azure'
+
+tofu workspace select "$ENV" || tofu workspace new "$ENV"
+tofu init # ensures the correct versions are downloaded.
+tofu apply -var-file="environment.${ENV}.tfvars" 
